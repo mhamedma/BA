c4b7f1d50071b50b225e25216ef0d5b3fcef99d0
RTLAB <moatamrimohamed@yahoo.com>
2024-04-26 15:56:01 +0200

    first commit

---
+++ launch/rsp.launch.py
@@ -0,0 +1,62 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.substitutions import LaunchConfiguration, Command
+from launch.actions import DeclareLaunchArgument
+from launch_ros.actions import Node
+
+import xacro 
+
+
+def generate_launch_description():
+
+    
+    use_sim_time = LaunchConfiguration('use_sim_time')
+    
+
+    
+    pkg_path = os.path.join(get_package_share_directory('robot'))
+    xacro_file = os.path.join(pkg_path,'description','robot.urdf.xacro')
+    robot_description_config = xacro.process_file(xacro_file)
+
+    robot_description_config = Command(['xacro ', xacro_file, ' sim_mode:=', use_sim_time])
+
+      
+    # Create a robot_state_publisher node
+    params = {'robot_description': robot_description_config, 'use_sim_time': use_sim_time}
+    node_robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[params]
+    )
+
+    rviz2_node = Node(
+         package="rviz2",
+          
+        executable="rviz2",
+        
+     )
+    
+    
+    node_joint_state_publisher_gui = Node(
+          package='joint_state_publisher_gui',
+         executable ='joint_state_publisher_gui',
+         output='screen',
+        
+     )
+    #launch 
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='false',
+            description='Use sim time, true or false'),
+        
+        
+         node_robot_state_publisher,
+         node_joint_state_publisher_gui,
+         rviz2_node,
+    ])
